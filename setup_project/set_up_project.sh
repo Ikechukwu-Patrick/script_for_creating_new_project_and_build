954d338155f1a29fcc88eb8f5181715fa16e94e9
Ike <patrickikechukwukalukalu@gmail.com>
2024-11-15 15:14:46 +0100

    Updated scripts to use main branch

--- setup_project/set_up_project.sh
+++ setup_project/set_up_project.sh
@@ -96,7 +96,7 @@ gh api -X PUT "/repos/$GITHUB_USERNAME/$PROJECT_NAME/branches/master/protection"
   "enforce_admins": true,
   "required_pull_request_reviews": {
     "dismiss_stale_reviews": true,
-    "require_code_owner_reviews": false,
+    "require_code_owner_reviews": true,
     "required_approving_review_count": 1
   },
   "restrictions": null
